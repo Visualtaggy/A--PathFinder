f45b3f84146ca1163ad5245c8fa8833d1f5bbb2f
VisualTaggy <visualtaggy@gmail.com>
2021-09-03 16:01:33 +0530

    +created node object

--- script.py
+++ script.py
@@ -4,3 +4,21 @@ from queue import PriorityQueue
 
 width = 800
 WINDOW = pygame.display.set_mode((width,width))
+pygame.display.set_caption("A* PathFinder - by Vishal Tyagi")
+
+colors = {
+    'white':(255,255,255),
+    }
+
+
+class Node:
+    def __init__(self,row,col,width,t_rows):
+        self.row = row
+        self.col = col
+        self.x = row * width
+        self.y = col * width
+        self.color = colors['white']
+        self.neighbours = []
+        self.width = width
+        self.total_rows = t_rows
+        
