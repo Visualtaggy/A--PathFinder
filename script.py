ac9ba39fc5ab0fda61b7d5f0847ee67eebf3c1bd
VisualTaggy <visualtaggy@gmail.com>
2021-09-03 16:24:57 +0530

    +functionality  to change box state

--- script.py
+++ script.py
@@ -8,6 +8,10 @@ pygame.display.set_caption("A* PathFinder - by Vishal Tyagi")
 
 colors = {
     'white':(255,255,255),
+    'blue':(64,206,227),
+    'navy':(12,53,71),
+    'purple':(466,8,99),
+    'orange':(248,54,0)
     }
 
 
@@ -22,3 +26,21 @@ class Node:
         self.width = width
         self.total_rows = t_rows
         
+    def find_position(self):
+        return self.row,self.col
+
+    def is_visited(self):
+        return self.color == colors['blue']
+    
+    def is_blocked(self):
+        return self.color == colors['navy']
+
+    def is_start(self):
+        return self.color == colors['purple']
+    
+    def is_destination(self):
+        return self.color == colors['orange']
+
+    def reset_board(self):
+        self.color == colors['white']
+    
